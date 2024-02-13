bc52f73ea940c8afce274754aa8fdb75285c870c
Nessaj44 <johnedmoncoc@gmail.com>
2024-02-13 20:13:31 +0800

    Add v2 link

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import RenderData from './renderData'
 
 function App() {
   return (
-    <>
+    <main>
+      <p style={{textAlign: "center"}}>The API is currently down. Please check out the <a href="https://65cb3b73ad3addaa01fbe67b--admirable-daffodil-91cd5b.netlify.app/">v2 version</a> of the application.</p>
       <CovidData render={(data) => <RenderData data={data} />} />
-    </>
+    </main>
   )
 }
 
